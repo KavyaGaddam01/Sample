40a12a4310ee37cbc5231421395c93fe9639a662
QSP <QSP@DESKTOP-56QICGD>
2025-06-13 14:30:27 +0530

    New class Sample committed

--- src/test/java/practice1/Sample.java
+++ src/test/java/practice1/Sample.java
@@ -1,11 +1,10 @@
-package practice;
+package practice1;
 
-public class Demo {
+public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.println("Hello World");
-  
+
 	}
 
 }
